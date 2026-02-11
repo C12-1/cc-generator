7e7882e06d4d710cadd502a9d455367b89344920
ctwelve <hisokaff88@gmail.com>
2026-02-11 22:05:29 +0100

    add the luhn algo

--- ccgen.py
+++ ccgen.py
@@ -12,7 +12,8 @@ print("""\n\n \033[34m██████╗██╗    ██╗█████
  ╚═════╝ ╚══╝╚══╝    ╚═╝   ╚══════╝╚══════╝ ╚═══╝  ╚══════╝\n\t\t\t\t\033[31m MADE BY : @C12\n \033[0m""")
 def checking():
     
-    bin_input = input("[+] write the path of the file (txt) \n[+] write the bin ex:456654|xx|xx|xxx , 456654 >> ").strip()
+
+    bin_input = input("\033[31m[+] write the path of the file (txt) \n[+] write the bin ex:456654|xx|xx|xxx , 456654 >> ").strip()
     try :
         
         if os.path.exists(bin_input) and os.path.isfile(bin_input):
@@ -42,21 +43,25 @@ def shufling():
     return choice
 def firts_num(num):
     the_num = num.split("|")
-    if 1 <= len(the_num) <= 4 and   int(the_num[0]):
-        cnum = the_num[0]
-        if 6 <= len(cnum) < 16:
-            xlen = 16 - len(cnum)
-            for _ in range(xlen):
-                cnum += shufling()
-                if len(cnum) == 16:
-                    return cnum
-        elif len(cnum) == 16:
-            return cnum
-        elif len(cnum) > 16:
-            
-            return False
-    else:
-        print("none")
+    try:
+        if 1 <= len(the_num) <= 4 and   int(the_num[0]):
+            cnum = the_num[0]
+            if 6 <= len(cnum) < 16:
+                xlen = 16 - len(cnum)
+                for _ in range(xlen):
+                    cnum += shufling()
+                    if len(cnum) == 16:
+                        return cnum
+            elif len(cnum) == 16:
+                return cnum
+            elif len(cnum) > 16:
+                
+                return False
+        else:
+            print("none")
+    except ValueError as e:
+        print( f"❗invalid bin number ~~ {num} ~~")
+        return False
         
 def month_year(num):
     full = num.split("|")
@@ -123,49 +128,81 @@ def cvv(num):
         for _ in range(3):
             cvv += shufling()
         return cvv
+
+def luhn_algo(number: str) -> bool:
+    luhn_num = number.split("|")[0][::-1]
+    total = 0
+    for i, x in enumerate(luhn_num):
+        x = ord(x) - 48
+        if i % 2 == 1:
+            x = x*2
+            if x > 9 :
+                x = (x % 10) + 1
+                
+        total += x
+    return total % 10 == 0 
+
 def generating(number):
     if number:
-        amount = input("how many cards default 10 >> ")
+        amount = input("How many cards default 10 >> ")
         
         def excuting():
             if type(number) == list:
                 
-                with open(f"{datetime.now().strftime("%Y-%m-%d %H-%M-%S")} CCs.txt" , "w") as file:
+                with open(f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')} CCs.txt" , "w") as file:
                     for card in range(len(number)):
                         for _ in range(int(amount)):
                                 format = f"{firts_num(number[card])}|{month_year(number[card])}|{cvv(number[card])}\n"
                                 if not firts_num(number[card]):
-                                    print(f"\033[31mbin length is out of range(6-16) : {number[card]}")
+                                    print("\033[31Mbin length is out of range(6-16)")
+                                
                                     continue
+                                while not luhn_algo(format):
+                                    format = f"{firts_num(number[card])}|{month_year(number[card])}|{cvv(number[card])}\n"
+                                   
                                 file.write(format)
                                 print(f"\033[34m {format.strip()}")
-            elif int(amount):
-                with open(f"{datetime.now().strftime("%Y-%m-%d %H-%M-%S")} CCs.txt" , "w") as file:
+            elif amount:
+                with open(f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')} CCs.txt" , "w") as file:
                     for _ in range(int(amount)):
                         format = f"{firts_num(number)}|{month_year(number)}|{cvv(number)}\n"
+
+                        while not luhn_algo(format) :
+                            format = f"{firts_num(number)}|{month_year(number)}|{cvv(number)}\n"
+                            
                         file.write(format)
                         print(f"\033[34m {format.strip()}")
+                       
+                                
         if amount.startswith(tuple("0123456789")):
             excuting()
         else:
             print("\033[31mwrite a correct number")
+            return "Write a correct number"
+            
     else:
         print("\033[31mwrong input")
+        return "Wrong input"
+
     
     
         
    
 def keep_going():
-    generating(checking())
-    while True:
-        answer = input("to keep generating 'yes' || to exit 'no' >> ").upper()
-        if answer == "YES":
-            generating(checking())
-        elif answer == "NO":
-            print("\033[33mthank you for using our script :)")
-            break
-        else:
-            print("write yes or no")
+    func = generating(checking())
+    if func == "write a correct number"  or func == "wrong input":
+        while True:
+            func: None | str = generating(checking())
+    else:
+        while True:
+            answer = input("To keep generating 'yes' || to exit 'no' >> ").upper()
+            if answer == "YES":
+                generating(checking())
+            elif answer == "NO":
+                print("\033[33m🔻Thank you for using our script :)")
+                break
+            else:
+                print("Write yes or no")
 
 if __name__ == "__main__":
     keep_going()
