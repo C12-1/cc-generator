6dacc029f56397c0af638ee2d04211e87793e299
ctwelve <hisokaff88@gmail.com>
2026-03-07 19:28:45 +0100

    fixing the algorith of the script

--- ccgen.py
+++ ccgen.py
@@ -89,10 +89,17 @@ def first_num(bin_number : str) -> str :
     if not bin_number:
         return False
     card_number = bin_number.strip().split("|")[0]
-    if not card_number.isdigit() and not 6 <= len(card_number) <= 16 or not card_number.startswith(tuple("4563")):
+        # Check if card_number is valid
+    if not card_number.isdigit():
+        UNWANTED_INPUTS += bin_number + "\n"
+        print(f"{RED}Invalid Bin : must contain only digits")
+        return False
+    
+    if not 6 <= len(card_number) <= 16 or not card_number.startswith(tuple("4563")):
         UNWANTED_INPUTS += bin_number + "\n"
         print(f"{RED}Invalid Bin : out of range or wrong must start with 5-4-6-3")
         return False
+
     if card_number.startswith(tuple("456")):
         while len(card_number) < 15:
             card_number += shufling()
@@ -111,10 +118,21 @@ def first_num(bin_number : str) -> str :
                 number = (number % 10) + 1
         total += number
     checksum = (10 - (total % 10)) % 10
-    return card_number + str(checksum)
+    final_number =  card_number + str(checksum)
+        # Final length validation
+    if card_number.startswith(tuple("456")) and len(final_number) != 16:
+        UNWANTED_INPUTS += bin_number + "\n"
+        
+        return False
+    elif card_number.startswith("3") and len(final_number) != 15:
+        UNWANTED_INPUTS += bin_number + "\n"
+        
+        return False
+    return final_number
 
 # handling month an year 
 def month_year(bin_number):
+
     #handling with month
     parts = bin_number.split("|")
     bin_month = ""
@@ -136,9 +154,9 @@ def month_year(bin_number):
         year = parts[2]
         if year.isdigit():
             if len(year) == 2:
-                year = f"20{year}"
+                bin_year = f"20{year}"
             elif len(year) == 4:
-                year = year
+                bin_year = year
             else:
                 bin_year = str(random.randint(CURRENT_YEAR, LEGAL_YEAR))
         else: 
@@ -148,6 +166,7 @@ def month_year(bin_number):
 
     return f"{bin_month}|{bin_year}"
 
+# cvv handling 
 def cvv(num:str) -> str:
     parts = num.split("|")
     if parts[0].startswith("3"):
@@ -168,7 +187,7 @@ def cvv(num:str) -> str:
         bin_cvv = "".join(shufling() for _ in range(cvv_length))
     return bin_cvv
     
-
+# checking the luhn validation
 def luhn_algo(number: str) -> bool:
     luhn_num = number.split("|")[0][::-1]
     total = 0
@@ -186,7 +205,6 @@ def luhn_algo(number: str) -> bool:
 def main(number:str): #-path : list or single bin number
     global UNWANTED_INPUTS
     filename = f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')} CCs.txt"
-    generated = 0
     
     if number :
         amount = input("How many cards default 10 >> ").strip()
@@ -197,7 +215,7 @@ def main(number:str): #-path : list or single bin number
  
         amount = int(amount)
         with open(filename , "w") as file:
-            # handling with list 
+            # handling with list || file path
             if isinstance(number , list) :
                 valid_bins = [] 
                 bin_list = number
@@ -205,13 +223,15 @@ def main(number:str): #-path : list or single bin number
                     card_number = first_num(bin)
                     if card_number and luhn_algo(card_number):
                         valid_bins.append(bin)
+                    else:
+                        UNWANTED_INPUTS += bin
                 if not valid_bins :
                     print(f"{RED}No valid bin found in the file")
                     return 
-                for i , bin in enumerate(valid_bins):
-                    while generated < amount:
-                        bin_number_parts = valid_bins[i]
 
+                for bin_number_parts in valid_bins:
+                    generated = 0
+                    while generated < amount:
                         first_number = first_num(bin_number_parts)
                         if first_number and luhn_algo(first_number):
                             month_year_bin = month_year(bin_number_parts)
@@ -221,11 +241,14 @@ def main(number:str): #-path : list or single bin number
                             file.write(format+ "\n")
                             print(f"{RED}{format}")
                             generated  += 1
+                    
                 if UNWANTED_INPUTS:
+                    UNWANTED_INPUTS = list(set(UNWANTED_INPUTS))
                     print(f"{RED}These are wrong inputs \n{UNWANTED_INPUTS}")
                 return "done"
             # handling with signle bin
             else:
+                generated = 0
                 while generated < amount:
                     bin_number_parts = number
                     first_number = first_num(bin_number_parts)
@@ -237,12 +260,12 @@ def main(number:str): #-path : list or single bin number
                         file.write(format+ "\n")
                         print(f"{RED}{format}")
                         generated  += 1
-                if UNWANTED_INPUTS:
-                    print(f"{RED}These are wrong inputs \n{UNWANTED_INPUTS}")
+                    else:
+                        return False
                 return "done"
     else: 
         return False
-
+# this function to ask if keep generating or not 
 def keep_going():
     while True:
         func = main(checking())
